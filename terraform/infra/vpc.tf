92f1de744505f634cd1054830f7d8c8b0fe9de20
erryasri <erryasricareer@gmail.com>
2026-02-04 07:10:16 +0800

    feat: add infra and juiceshop

---
+++ terraform/infra/vpc.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(var.tags, {
+    Name = "Caprover-VPC"
+  })
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
